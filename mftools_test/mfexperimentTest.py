2957684aba6dd4c52287f753e56e479259346aae
saanvimg <sgireesh@ucsd.edu>
2025-07-21 16:41:45 -0700

    Working on mfexperimentTest.py!

--- mftools_test/mfexperimentTest.py
+++ mftools_test/mfexperimentTest.py
@@ -3,6 +3,7 @@ from mftools import mfexperiment
 from mftools.segmentation import CellSegmentation
 import scanpy as sc
 import numpy as np
+from pathlib import Path # used from mfexperiment (helps me with the paths)
 
 class MSEperiment_Test(unittest.TestCase): 
     
@@ -26,9 +27,6 @@ class MSEperiment_Test(unittest.TestCase):
         self.assertTrue(all(~adata.obs['global_volume'].isna()))
         self.assertTrue(np.all(np.mod(adata.X, 1) == 0))
 
-   
-
-
 
     def test__images(self):
         pass
@@ -47,6 +45,34 @@ class MSEperiment_Test(unittest.TestCase):
     def test__images(self):
         pass
 
+# Creating a class for unit tests for a regular merscope experiment
+# Inherits the other tests from MSEperiment_Test
+class Test_Regular_MSExperiment(MSEperiment_Test): 
+    
+    # manually creating a directory to use for the test
+    def setUp(self):
+        # note -> change the test_regular_exp to an ACTUAL experiment (maybe?)
+        self.test_dir = Path("test_regular_exp") 
+        self.e.savepath = self.test_dir # making sure I use this path in the test
+
+        # TODO: create a dataframe (or use our own to load in to check)
+
+    # testing necessary functions -> cell_by_gene (should work regardless)
+    def test_cell_by_gene(self):
+        self.e.save_cell_by_gene_table() # add the dataframe we're using in here
+        loaded = self.e.load_cell_by_gene_table() # loading the dataframe
+        self.assertTrue((self.savepath / "cell_by_gene.csv").exists()) # making sure this exists
+        # TODO: I need to make sure the contents of the load are correct too
+
+
+    # test an interesting function (adjust_spatial_coordinates)
+    # note: only in regular merscope experiment (important!)
+    def test_adjust_spatial_coordinates(self):
+        adata:sc.AnnData = self.e.adjust_spatial_coordinates() # wrong -> fix
+
+    # checking for the metadata - could be helpful
+    def test_cell_metadata(self):
+        pass
 
 
 if __name__ == "__main__":
