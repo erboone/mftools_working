fb36fc52a538963fedd602fdb982c555df7eedef
erboone <eboone1001@gmail.com>
2025-08-25 15:09:00 -0700

    plotting up to date!

--- mftools/plotting.py
+++ mftools/plotting.py
@@ -522,14 +522,13 @@ def celltype_corr(mer_adata, ref_adata, ctkey, lineage_order=None):
         mer_ctkey, ref_ctkey = (ctkey, ctkey) 
 
     mer_adata = mer_adata.copy()
-    # mer_adata.X = mer_adata
-    dend = sc.tl.dendrogram(mer_adata, groupby='_CELLTYPE', inplace=False)
-    print(dend)
+    mer_adata.X = mer_adata.layers['counts']
 
     if lineage_order:
         celltypes = lineage_order
     else:
-        celltypes = dend["categories_ordered"]
+        celltypes = mer_adata.obs[mer_ctkey].dropna().unique()
+    sizes = {}
     mer_counts = {}
     ref_counts = {}
 
@@ -542,26 +541,34 @@ def celltype_corr(mer_adata, ref_adata, ctkey, lineage_order=None):
             continue
         mer_subdata = temp_mer_adata[temp_mer_adata.obs[mer_ctkey] == ct]
         ref_subdata = temp_ref_adata[temp_ref_adata.obs[ref_ctkey] == ct]
+        sizes[ct] = mer_subdata.shape[0]
+        common = mer_subdata.var_names.intersection(ref_subdata.var_names)
 
-        print(mer_subdata.shape[0] < 100, ref_subdata.shape[0] < 100)
         if mer_subdata.shape[0] < 1 or ref_subdata.shape[0] < 1:
             continue
         mer_counts[ct] = np.array(mer_subdata.X.mean(axis=0)).flatten()
         ref_counts[ct] = np.array(ref_subdata.X.mean(axis=0)).flatten()
 
-
+    print(celltypes)
+    print(mer_subdata.shape)
+    print(ref_subdata.shape)
+    print(mer_counts.keys())
+    print(ref_counts.keys())
+    print(mer_counts)
+    print(ref_counts)
     result = []
     for ct1 in mer_counts.keys():
         row = []
         for ct2 in ref_counts.keys():
             row.append(
                 pearsonr(
-                    np.nan_to_num(mer_counts[ct1]), 
-                    np.nan_to_num(ref_counts[ct2])
+                    zscore(mer_counts[ct1]), 
+                    zscore(ref_counts[ct2])
             )[0])
         result.append(row)
 
     print(result)
+    labels = [f"{ct} ({sizes[ct]:,d} cells)" for ct in mer_counts.keys()] 
     result = pd.DataFrame(result, index=mer_counts.keys(), columns=ref_counts.keys())
 
     import seaborn as sns
@@ -571,16 +578,12 @@ def celltype_corr(mer_adata, ref_adata, ctkey, lineage_order=None):
     #medians = [np.median(data[d1].X.sum(axis=1)) for d1 in data]
     #cmap = matplotlib.cm.get_cmap('viridis')
     #norm = matplotlib.colors.Normalize(vmin=min(medians), vmax=sorted(medians)[-2])
-   
-
-    fig = sns.heatmap(result, 
-                cmap="coolwarm",# row_cluster=False, col_cluster=False, 
-                # order=dend.keys(),
-                #dendrogram_ratio=(0, 0.1), cbar_pos=(1, 0.15, .03, .7),
+    fig = sns.clustermap(result, 
+                cmap="coolwarm", row_cluster=False, col_cluster=False, 
+                dendrogram_ratio=(0, 0.1), cbar_pos=(1, 0.15, .03, .7),
                 vmin=-1, vmax=1,
-                annot=False, fmt=".2f", cbar_kws={"label": "Correlation of transcripts per gene"}, annot_kws={"size": 14, "weight": "bold"})# col_colors=[cmap(norm(med)) for med in medians], 
-    plt.xticks(size='x-small')
-    plt.xticks(size='x-small')
+                annot=False, fmt=".2f", cbar_kws={"label": "Correlation of transcripts per gene"}, 
+                figsize=(15, 15), annot_kws={"size": 14, "weight": "bold"})# col_colors=[cmap(norm(med)) for med in medians], 
     return fig
     
 def _plot_correlation(data1:pd.Series, data2:pd.Series, ax1:str, ax2:str, highlight, logscale:bool=False):
